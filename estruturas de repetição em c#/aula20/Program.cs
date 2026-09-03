453a43e7a7ab4a8cc05503538e11167fda5ac377
Joao <lzgit892@gmail.com>
2026-09-03 00:25:18 -0300

    novos exercicios de estruturas de repetição em c#.

--- estruturas de repetição em c#/aula20/Program.cs
+++ estruturas de repetição em c#/aula20/Program.cs
@@ -39,19 +39,62 @@
 
 // DESAFIO 01
 
-Console.WriteLine("Jogador desafiante insira um valor numérico:");
-int desafiante = int.Parse(Console.ReadLine());
+// Console.WriteLine("Jogador desafiante insira um valor numérico:");
+// int desafiante = int.Parse(Console.ReadLine());
 
-Console.Clear();
+// Console.Clear();
 
-Console.WriteLine("Jogador desafiado informe um valor: ");
-int desafiado = int.Parse(Console.ReadLine());
+// Console.WriteLine("Jogador desafiado informe um valor: ");
+// int desafiado = int.Parse(Console.ReadLine());
 
-while(desafiante != desafiado)
+// while(desafiante != desafiado)
+// {
+//     Console.WriteLine("Você errou! Insira um novo valor");
+//     desafiado = int.Parse(Console.ReadLine());
+// }
+
+// Console.WriteLine("Sucesso!");
+
+
+
+
+
+// EXERCICIO CURSO - LISTA 6 - 11
+// Console.WriteLine("Informe a nota do aluno: ");
+// int nota = int.Parse(Console.ReadLine());
+//  while(nota<0 || nota>10)
+// {
+//     Console.WriteLine("Nota inválida! Informe a nota entre 0 e 10: ");
+//     nota = int.Parse(Console.ReadLine());
+// }
+
+
+// EXERCICIO CURSO - LISTA 6 - 12
+// Console.WriteLine("Informe a senha: ");
+// int senha = int.Parse(Console.ReadLine());
+// while(senha != 1234)
+// {
+//     Console.WriteLine("Senha incorreta! Informe a senha: ");
+//     senha = int.Parse(Console.ReadLine());
+// }
+// Console.WriteLine("Acesso liberado!");
+
+
+
+// EXERCICIO CURSO - LISTA 6 - 13
+
+int cont = 0;
+int negativo = 0;
+while(cont < 10)
 {
-    Console.WriteLine("Você errou! Insira um novo valor");
-    desafiado = int.Parse(Console.ReadLine());
-}
+    Console.WriteLine("Informe um número: ");
+    int numero = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Sucesso!");
+    if(numero < 0)
+    {
+        negativo++;
+    }
+    cont++;
+}
+Console.WriteLine("Quantidade de números negativos: " + negativo);
 
