168a21c95e3ec8398e32fd32bf486f5c71300c92
Joao <lzgit892@gmail.com>
2026-09-03 23:09:32 -0300

    adicionando comments

--- aula 17 - operadores logicos em c#/aula17/Program.cs
+++ aula 17 - operadores logicos em c#/aula17/Program.cs
@@ -1,8 +1,8 @@
 ﻿Console.WriteLine("Escreva as notas: ");
-int n1 = int.Parse(Console.ReadLine);
-int n2 = int.Parse(Console.ReadLine);
-int n3 = int.Parse(Console.ReadLine);
-int n4 = int.Parse(Console.ReadLine);
+int n1 = int.Parse(Console.ReadLine());
+int n2 = int.Parse(Console.ReadLine());
+int n3 = int.Parse(Console.ReadLine());
+int n4 = int.Parse(Console.ReadLine());
 
 int media = (n1 + n2+ n3 + n4) / 4;
 
